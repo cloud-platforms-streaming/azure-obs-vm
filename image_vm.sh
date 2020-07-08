49ef25960f2253df008a110b011b4fd33c34e123
Erik M Jacobs <erikmjacobs@gmail.com>
2020-07-08 12:49:09 -0400

    tweaks to bash scripts
    * moves echo of RG to end instead of beginning
    * adds param for deploying image

--- image_vm.sh
+++ image_vm.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 export RG=`uuidgen`
-echo "RG is ${RG}"
 export NAME=`date +"%m%d%y%H%M"`
 export LOCATION=westus2
 
+if [ -z "$1" ]
+then
+  export IMAGEREFERENCE="-p imageReference=$1"
+fi
+
 az group create --name ${RG} --location ${LOCATION} --verbose
-az deployment group create -g ${RG} -f imaged_vm_template.json -p parameters.json  -p adminPassword=${PASSWORD} -n ${NAME} --verbose
+az deployment group create -g ${RG} -f imaged_vm_template.json -p parameters.json  -p adminPassword=${PASSWORD} $IMAGEREFERENCE -n ${NAME} --verbose
+
+echo "RG is ${RG}"
