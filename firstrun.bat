dc665452630bf3531ac3d55196d9d558cd284f65
Erik M Jacobs <erikmjacobs@gmail.com>
2020-07-08 10:07:11 -0400

    changes to clone windows branch

--- firstrun.bat
+++ firstrun.bat
@@ -1,5 +1,5 @@
 rd c:\streaming-tools /s /q
-"C:\Program Files\Git\bin\git.exe" clone https://github.com/cloud-platforms-streaming/streaming-tools c:\streaming-tools
+"C:\Program Files\Git\bin\git.exe" clone -b windows-vm https://github.com/cloud-platforms-streaming/streaming-tools c:\streaming-tools
 rd C:\Users\streamer\AppData\Roaming\obs-studio\ /S /Q
 mkdir C:\Users\streamer\AppData\Roaming\obs-studio\
 xcopy /E c:\streaming-tools\obs\openshift_streaming\* c:\Users\streamer\AppData\Roaming\obs-studio\
