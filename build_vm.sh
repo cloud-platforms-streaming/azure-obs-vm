1b4e1719880a4afd5b1695230cba8e6092d4bf7d
Erik M Jacobs <erikmjacobs@gmail.com>
2020-10-29 16:33:27 -0400

    change default build location to eastus

--- build_vm.sh
+++ build_vm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export RG=`uuidgen`
 export NAME=`date +"%m%d%y%H%M"`
-export LOCATION=westus2
+export LOCATION=eastus
 
 az group create --name ${RG} --location ${LOCATION} --verbose
 az deployment group create -g ${RG} -f build_vm_template.json -p parameters.json  -p adminPassword=${PASSWORD} -n ${NAME} --verbose
